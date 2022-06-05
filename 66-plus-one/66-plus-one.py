dbc71f556670c1bcec803847c2662753a696bbaa
Aniket Kale <aniketk746@gmail.com>
2022-06-04 20:54:17 -0400

    Time: 50 ms (37.12%), Space: 13.8 MB (57.73%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,10 +1,32 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
+        dcopy = digits
+        i=len(digits)-1
+        while i>=0 and digits[i]==9:
+            i-=1
         
-        num = int(''.join([str(d) for d in digits]))
+        if i == len(digits)-1:
+            tmp = digits[i] + 1
+            return digits[:i] + [tmp]
+        else:
+            zeros = [0]*(len(digits)-1-i)
+            
+            if i >= 0:
+                digits[i] += 1
+                return digits[:i+1] + zeros
+            else:
+                return [1]+zeros
         
-        num +=1
         
-        arr = [int(n) for n in str(num)]
+#         num = int(''.join([str(d) for d in digits]))
         
-        return arr
+#         num +=1
+        
+#         arr = [int(n) for n in str(num)]
+        
+#         return arr
+
+
+        
+    
+        
