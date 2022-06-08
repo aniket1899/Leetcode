5a1c50047597600ba03d90d0d70ecc6c26987390
Aniket Kale <aniketk746@gmail.com>
2022-06-07 20:59:03 -0400

    Time: 65 ms (57.54%), Space: 14.5 MB (34.46%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        lens = len(s)
+        lent = len(t)
+        
+        if lens != lent:
+            return False
+        counts = {}
+        for i in range(lens):
+            if s[i] in counts:
+                counts[s[i]] += 1
+            else:
+                counts[s[i]] = 1
+            if t[i] in counts:
+                counts[t[i]] -= 1
+            else:
+                counts[t[i]] = -1
+        # print(counts.values())
+        return not any(counts.values())
+                
+            
+        
