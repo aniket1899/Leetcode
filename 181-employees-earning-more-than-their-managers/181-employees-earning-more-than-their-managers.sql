ea7e35744520862fbf4ac019c04fcb6038abc8f1
Aniket Kale <aniketk746@gmail.com>
2022-06-23 15:42:30 -0400

    Time: 597 ms (87.82%), Space: 0B (100.00%) - LeetHub

--- 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
@@ -1,5 +1,7 @@
 /* Write your PL/SQL query statement below */
-select name as Employee from Employee emp
-where
-    exists (select 1 from Employee mgr 
-           where emp.managerId = mgr.id and mgr.salary < emp.salary);
+-- select name as Employee from Employee emp
+-- where
+--     exists (select 1 from Employee mgr 
+--            where emp.managerId = mgr.id and mgr.salary < emp.salary);
+
+select e.name as Employee from Employee e inner join Employee m on e.managerId = m.id and e.salary > m.salary;
