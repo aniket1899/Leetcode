90ee605a0bf0b9f51ca508085c041ca516aacd3c
Aniket Kale <aniketk746@gmail.com>
2022-07-05 12:17:18 -0400

    Time: 347 ms (51.29%), Space: 25.5 MB (27.91%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -5,21 +5,19 @@ class Solution:
         """
         N = len(nums)
         k = k%N
-        # nums = nums[-k:] + nums[:-k-1]
-        # tmp = nums.copy()
-        # nums = []
-        # nums = tmp[N-k-1:] + tmp[:N-k-1]
+        
+        nums[:] = nums[N-k:] + nums[:N-k]
         
         # for _ in range(k):
         #     tmp = nums.pop()
         #     nums.insert(0, tmp)
         
-        if k <= N - k:
-            for _ in range(k):
-                tmp = nums.pop()
-                nums.insert(0, tmp)
-        else:
-            for _ in range(N-k):
-                tmp = nums.pop(0)
-                nums.append(tmp)
+        # if k <= N - k:
+        #     for _ in range(k):
+        #         tmp = nums.pop()
+        #         nums.insert(0, tmp)
+        # else:
+        #     for _ in range(N-k):
+        #         tmp = nums.pop(0)
+        #         nums.append(tmp)
         # print(nums)
