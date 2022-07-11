6f825d3472f3b21b6294c538411eefb3bda25794
Aniket Kale <aniketk746@gmail.com>
2022-07-10 20:31:57 -0500

    Time: 830 ms (50.90%), Space: 36.5 MB (7.66%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.py
@@ -2,10 +2,6 @@ from heapq import heappop, heappush
 class MedianFinder:
 
     def __init__(self):
-        # self.nums = []
-        # self.minheap =  [] # negative values
-        # self.maxheap = []
-        
         self.maxHeap = []
         self.minHeap = []
         
@@ -18,19 +14,6 @@ class MedianFinder:
         else:
             heappush(self.maxHeap, num)
             heappush(self.minHeap, -heappop(self.maxHeap))
-#         if len(self.minheap) == len(self.maxheap):
-#             if not self.maxheap or self.maxheap[0] <= num:
-#                 heappush(self.maxheap, num)
-#             else:
-#                 heappush(self.maxheap, -heappop(self.minheap))
-#                 heappush(self.minheap, -num)
-                
-#         else:
-#             if self.maxheap[0] <= num:
-#                 heappush(self.minheap, -heappop(self.maxheap))
-#                 heappush(self.maxheap, num)
-#             else:
-#                 heappush(self.minheap, -num)
         
 
     def findMedian(self) -> float:
@@ -39,23 +22,7 @@ class MedianFinder:
         else:
             return float(self.maxHeap[0])
         
-        
-        # if len(self.upperHeap) == len(self.lowerHeap):
-        #     upperMin = + self.upperHeap[0]
-        #     lowerMax = - self.lowerHeap[0]
-        #     return ( float(upperMin) + float(lowerMax) ) / 2.0
-        # else:
-        #     return float(self.upperHeap[0])
-        # print(self.minheap ,self.maxheap)
-        # if len(self.maxheap) == len(self.minheap) + 1:
-        #     return float(self.maxheap[0])
-        # # elif len(self.maxheap) == len(self.minheap):
-        # else:
-        #     return (self.maxheap[0]-self.minheap[-1]) / 2
-            
-            
-        
-
+    
 
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
