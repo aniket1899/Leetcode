b82994945886c5334984e4107ba99fbac47ab369
Aniket Kale <aniketk746@gmail.com>
2022-07-15 12:57:32 -0500

    Time: 62 ms (21.91%), Space: 13.9 MB (55.59%) - LeetHub

--- 342-power-of-four/342-power-of-four.py
+++ 342-power-of-four/342-power-of-four.py
@@ -1,15 +1,30 @@
 class Solution:
+    def powFour(self, n):
+        if n == 1:
+            return True
+        if n % 4:
+            return False
+        return self.powFour(n//4)
+        
+            
+        
     def isPowerOfFour(self, n: int) -> bool:
         # if n <= 1:
         #     return n == 1
         
         # print(binary)
-        ## Method 1: 
+        ## Method #1: 
         # binary = list(reversed(bin(n)[2:]))
-        # return binary.count('1') == 1 and binary.index('1') % 2 == 0
+        # return n>0 and binary.count('1') == 1 and binary.index('1') % 2 == 0
         
-        ## Method 2: 
-        binary = list(reversed(bin(n)[2:]))
-        return n>0 and n & (n-1) == 0 and binary.index('1') % 2 == 0        
+        ## Method #2: 
+        # binary = list(reversed(bin(n)[2:]))
+        # return n>0 and n & (n-1) == 0 and binary.index('1') % 2 == 0        
+        
+        ## Method #3:
+        if n <= 0:
+            return False
+        
+        return self.powFour(n)
         
         
