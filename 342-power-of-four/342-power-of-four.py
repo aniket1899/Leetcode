04099761edfd77f58c0edc7525be1939c18af3ba
Aniket Kale <aniketk746@gmail.com>
2022-07-15 12:36:42 -0500

    Time: 65 ms (16.96%), Space: 13.9 MB (9.99%) - LeetHub

--- 342-power-of-four/342-power-of-four.py
+++ 342-power-of-four/342-power-of-four.py
@@ -4,4 +4,5 @@ class Solution:
             return n == 1
         binary = list(reversed(bin(n)[2:]))
         # print(binary)
-        return binary.count('1') == 1 and binary.index('1') % 2 == 0
+        # return binary.count('1') == 1 and binary.index('1') % 2 == 0
+        return n & (n-1) == 0 and binary.index('1') % 2 == 0        
