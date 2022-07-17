3d85cc06aa5e8d823fd257ac9ebe53554c67dcdc
Aniket Kale <aniketk746@gmail.com>
2022-07-17 01:06:48 -0500

    Time: 57 ms (45.72%), Space: 13.9 MB (32.21%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,10 +1,19 @@
+from collections import Counter
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        s = sorted(s)
-        t = sorted(t)
-        skip = 0
-        for i, ch in enumerate(s):
-            if ch != t[i]:
-                return t[i]
+        
+        ctrS = Counter(s)
+        ctrT = Counter(t)
+        
+        for key, val in ctrT.items():
+            if key not in ctrS or val == ctrS[key] + 1:
+                return key
+        
+#         s = sorted(s)
+#         t = sorted(t)
+#         skip = 0
+#         for i, ch in enumerate(s):
+#             if ch != t[i]:
+#                 return t[i]
                 
-        return t[-1]   
+#         return t[-1]   
