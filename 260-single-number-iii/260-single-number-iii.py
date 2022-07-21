8883cad3b6a7ced9227e6de5ddbe3f92b0acdd40
Aniket Kale <aniketk746@gmail.com>
2022-07-21 13:30:31 -0500

    Time: 68 ms (87.46%), Space: 16.1 MB (31.77%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.py
+++ 260-single-number-iii/260-single-number-iii.py
@@ -1,20 +1,27 @@
+from collections import Counter
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
         
-        nums.sort()
-        i = 0
-        res = []
-        while i+1 < len(nums):
-            if nums[i] == nums[i+1]:
-                i += 2
-            else:
-                # print(i)
-                res.append(nums[i])
-                i += 1
+#         nums.sort()
+#         i = 0
+#         res = []
+#         while i+1 < len(nums):
+#             if nums[i] == nums[i+1]:
+#                 i += 2
+#             else:
+#                 # print(i)
+#                 res.append(nums[i])
+#                 i += 1
                 
-        if 1 == len(res):
-            res.append(nums[-1])
-            
+#         if 1 == len(res):
+#             res.append(nums[-1])
+         
+        res = []
+        ctr = Counter(nums)
+        for key, val in ctr.items():
+            if val == 1:
+                res.append(key)
+                
         return res
         
         
