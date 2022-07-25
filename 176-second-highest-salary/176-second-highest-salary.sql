9a85bcb4fbc9db530109ca2220f912b613b3fe26
Aniket Kale <aniketk746@gmail.com>
2022-07-25 14:49:59 -0500

    Time: 1249 ms (5.34%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,4 +1,4 @@
 /* Write your PL/SQL query statement below */
 
-SELECT max(q.salary) as SecondHighestSalary from (SELECT salary from Employee e1
-                        WHERE e1.salary <> (SELECT max(salary) FROM Employee)) q;
+SELECT max(e1.salary) as SecondHighestSalary  from Employee e1
+                        WHERE e1.salary <> (SELECT max(salary) FROM Employee);
