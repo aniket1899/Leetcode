1d40a184a1e498622d7101cf5e28f76aa6765414
Aniket Kale <aniketk746@gmail.com>
2022-08-01 23:21:13 -0500

    Time: 36 ms (88.38%), Space: 13.9 MB (58.85%) - LeetHub

--- 263-ugly-number/263-ugly-number.py
+++ 263-ugly-number/263-ugly-number.py
@@ -3,10 +3,10 @@ class Solution:
         if n == 0:
             return False
         uglies = [2, 3 , 5]
-        uglies = [(u, n % u) for u in [2, 3 , 5]]
+        # uglies = [(u, n % u) for u in [2, 3 , 5]]
         
-        for u, d in uglies:
-            if d == 0:
-                while n%u == 0:
-                    n = n//u
+        for u in uglies:
+            # if d == 0:
+            while n%u == 0:
+                n = n//u
         return n == 1
