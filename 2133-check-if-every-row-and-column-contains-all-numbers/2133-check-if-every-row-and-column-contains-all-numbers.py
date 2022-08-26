a3d0a2f2b880820c0dd672337b301f0904223738
Aniket Kale <aniketk746@gmail.com>
2022-08-26 15:37:53 -0400

    Time: 1004 ms (77.06%), Space: 14.4 MB (87.33%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
@@ -1,20 +1,24 @@
 from collections import defaultdict
 class Solution:
     def checkValid(self, matrix: List[List[int]]) -> bool:
-        
-        rowAddition  = set([i+1 for i in range(len(matrix))])
-        colAdditions = defaultdict(list)
-        for row in matrix:
-            if set(row) != rowAddition:
+        valid  = set([i+1 for i in range(len(matrix))])
+
+        for row, col in zip(matrix, zip(*matrix)):
+            if set(row) != valid or set(col) != valid:
                 return False
             
-            for k in range(len(row)):
-                colAdditions[k].append(row[k])
+#         colAdditions = defaultdict(list)
+#         for row in matrix:
+#             if set(row) != rowAddition:
+#                 return False
+            
+#             for k in range(len(row)):
+#                 colAdditions[k].append(row[k])
         
             
-        for idx, coladd in colAdditions.items():
-            if set(coladd) != rowAddition:
-                return False
+#         for idx, coladd in colAdditions.items():
+#             if set(coladd) != rowAddition:
+#                 return False
         
         return True
         
