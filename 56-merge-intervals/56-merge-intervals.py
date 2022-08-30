0cb90f2f7d1071249d9022f8acf23605e25640a3
Aniket Kale <aniketk746@gmail.com>
2022-08-29 22:10:08 -0400

    Time: 288 ms (17.86%), Space: 18 MB (85.02%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -2,16 +2,29 @@ from heapq import heappush, heappop, heapify
 from collections import deque
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        heapify(intervals)
-        merged = deque([heappop(intervals)])
-        openInt = True
-        while intervals:
-            start, end = heappop(intervals)
-            if start <= merged[-1][1]:
+#         heapify(intervals)
+#         merged = deque([heappop(intervals)])
+#         openInt = True
+#         while intervals:
+#             start, end = heappop(intervals)
+#             if start <= merged[-1][1]:
+#                 tmp = merged.pop()
+#                 merged.append([tmp[0], max(tmp[1], end)])
+#             else:
+#                 merged.append([start, end])
+                
+#         return merged
+
+        intervals.sort()
+        # print(intervals)
+    
+        merged = deque([intervals[0]])
+        
+        for interval in intervals[1:]:
+            if interval[0] <= merged[-1][1]:
                 tmp = merged.pop()
-                merged.append([tmp[0], max(tmp[1], end)])
+                merged.append([tmp[0], max(tmp[1], interval[1])])
             else:
-                merged.append([start, end])
+                merged.append(interval)
                 
         return merged
-                
