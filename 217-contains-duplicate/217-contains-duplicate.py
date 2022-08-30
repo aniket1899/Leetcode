16bdf70f58e179ff00a9a80c8f6a6762a1ab7b3b
Aniket Kale <aniketk746@gmail.com>
2022-08-30 18:25:20 -0400

    Time: 1074 ms (5.02%), Space: 26.1 MB (29.19%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,24 +1,26 @@
 from collections import defaultdict
 
 class Solution:
-    def solution_1(self, nums: List[int]) -> bool:
+#     def solution_1(self, nums: List[int]) -> bool:
         
-        ctr = defaultdict(int)
-        for num in nums:
-            if ctr[num] == 1:
-                return True
-            ctr[num] += 1
+#         ctr = defaultdict(int)
+#         for num in nums:
+#             if ctr[num] == 1:
+#                 return True
+#             ctr[num] += 1
             
-        return False
-    def solution_2(self, nums: List[int]) -> bool:
-        nums.sort()
-        prev = nums[0]
-        for num in nums[1:]:
-            if num == prev:
-                return True
-            prev = num
+#         return False
+    
+#     def solution_2(self, nums: List[int]) -> bool:
+#         nums.sort()
+#         prev = nums[0]
+#         for num in nums[1:]:
+#             if num == prev:
+#                 return True
+#             prev = num
         
-        return False
+#         return False
+    
     def solution_3(self, nums: List[int]) -> bool:
         ctr = set()
         
