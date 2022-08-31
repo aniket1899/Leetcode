94c4bb44b4812d6c5f6054197511dde6c6eba367
Aniket Kale <aniketk746@gmail.com>
2022-08-31 18:57:04 -0400

    Time: 59 ms (16.11%), Space: 13.9 MB (52.98%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.py
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.py
@@ -1,5 +1,33 @@
 class Solution:
-    def isSameAfterReversals(self, num: int) -> bool:
-        num = str(num)
-        return not num[-1] == '0' or num == '0'
+    def divide(self, dividend: int, divisor: int) -> int:
+        
+        counter = 0
+        
+        if dividend<0:
+            dvd_neg = -1
+        else:
+            dvd_neg = 1
+        
+        if divisor<0:
+            dvs_neg = -1
+        else:
+            dvs_neg = 1
+            
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+        
+        if divisor == 1:
+            counter = dividend
+        else:
+            while  dividend >= divisor:
+                dividend -= divisor
+                counter += 1
+            
+        if dvd_neg == -1:
+            counter = - counter
+        if dvs_neg == -1:
+            counter = - counter
+        if counter > 2147483647:
+            counter = 2147483647
+        return counter
         
