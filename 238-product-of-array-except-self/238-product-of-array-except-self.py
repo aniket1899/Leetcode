aac3554839897aaddaf3fa175749c54babb209b0
Aniket Kale <aniketk746@gmail.com>
2022-08-31 13:21:04 -0400

    Time: 250 ms (88.73%), Space: 21.6 MB (33.78%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,22 +1,46 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        # solution #2
+        
+        products = []
         prod = 1
-        prodZero = 1
-        zeros = 0
-        for n in nums:
-            if n == 0:
-                zeros += 1
-                prodZero = prod
-            else:
-                prodZero *= n
-            prod *= n
+        for num in nums:
+            products.append(prod)
+            prod *= num
         
-        res = []
+        print(products)
         
-        for n in nums:
-            if n == 0:
-                res.append(prodZero)
-            else:
-                res.append(prod//n)
-        return res
+        prod = 1
+        for i in reversed(range(len(nums))):
+            products[i] *= prod
+            prod *= nums[i]
+            
+            
+        return products
+            
+            
+            
+            
+            
+        # solution #1
+#         prod = 1
+#         prodZero = 1
+#         zeros = 0
+#         for n in nums:
+#             if n == 0:
+#                 zeros += 1
+#                 prodZero = prod
+#             else:
+#                 prodZero *= n
+#             prod *= n
+        
+#         res = []
+        
+#         for n in nums:
+#             if n == 0:
+#                 res.append(prodZero)
+#             else:
+#                 res.append(prod//n)
+#         return res
             
