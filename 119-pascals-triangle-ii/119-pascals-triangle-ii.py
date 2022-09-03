2f48e5c59b9d9c7f68b03188f07575909d89d637
Aniket Kale <aniketk746@gmail.com>
2022-09-03 12:54:34 -0400

    Time: 77 ms (5.01%), Space: 13.8 MB (63.26%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.py
@@ -5,7 +5,7 @@ class Solution:
             row = [1]*i
             for j in range(1, i-1):
                 row[j] = pascal[-1][j-1] + pascal[-1][j]
-            pascal = [pascal[-1]]
+            pascal = pascal[:-1]
             pascal.append(row)
         
         return pascal[-1]
