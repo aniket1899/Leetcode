9ac50d697cf60f5f21bc82cc3264314cb2990a51
Aniket Kale <aniketk746@gmail.com>
2022-09-11 21:06:16 -0400

    Time: 70 ms (12.11%), Space: 13.8 MB (70.50%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -4,25 +4,51 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def removeNthHelper(self, node, n):
-        if not node:
-            return 1, node
+#     def removeNthHelper(self, node, n):
+#         if not node:
+#             return 1, node
         
-        idx, curr = self.removeNthHelper(node.next, n)
-        # print(idx, node)
-        if idx == n + 1:
-            if node.next:
-                node.next = node.next.next
-            else:
-                node = None
+#         idx, curr = self.removeNthHelper(node.next, n)
+#         # print(idx, node)
+#         if idx == n + 1:
+#             if node.next:
+#                 node.next = node.next.next
+#             else:
+#                 node = None
         
-        return idx+1, node
+#         return idx+1, node
             
         
         
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        idx, node = self.removeNthHelper(head, n)
-        if idx-1 == n:
-            head = head.next
+#         idx, node = self.removeNthHelper(head, n)
+#         if idx-1 == n:
+#             head = head.next
             
+#         return head
+
+        node = head
+        stk = []
+        length = 0
+        while node:
+            stk.append(node)
+            node = node.next
+            length += 1
+        
+        if n == length:
+            head = head.next
+            return head
+        
+        # else
+        for _ in range(n):
+            stk.pop()
+        
+        if stk[-1].next:
+            stk[-1].next = stk[-1].next.next
+        else:
+            stk[-1].next = None
+        
         return head
+        
+        
+    
